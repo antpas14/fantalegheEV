f911647f7dbb682b309a3ebabea28e7166d26630
antonello <antonello.pastena@gmail.com>
2018-10-11 23:41:43 +0100

    Some refactoring and added travis yml

--- backend/src/main/java/org/gcnc/calculate/controller/CalculateController.java
+++ backend/src/main/java/org/gcnc/calculate/controller/CalculateController.java
@@ -2,6 +2,7 @@ package org.gcnc.calculate.controller;
 
 import org.gcnc.calculate.model.Request;
 import org.gcnc.calculate.model.Response;
+import org.gcnc.calculate.service.CalculateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
