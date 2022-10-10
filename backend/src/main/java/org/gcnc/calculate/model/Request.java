205dea7ba66120f5b467c60e3b59e7a1267a2ca9
antonello <antonello.pastena@gmail.com>
2022-10-10 20:07:43 +0100

    fix some tests and more refactor

--- backend/src/main/java/org/gcnc/calculate/model/Request.java
+++ backend/src/main/java/org/gcnc/calculate/model/Request.java
@@ -7,11 +7,9 @@ import java.io.Serializable;
 @Getter
 public class Request implements Serializable {
     private String leagueName;
-
     public Request(String leagueName) {
         this.leagueName = leagueName;
     }
-
     public static Request buildFor(String leagueName) {
         return new Request(leagueName);
     }
