69d0692b711f42c2bfca457b76a4557b476bfc25
antonello <antonello.pastena@gmail.com>
2022-10-30 16:42:39 +0000

    fixing dependencies

--- backend/src/main/java/org/gcnc/calculate/Application.java
+++ backend/src/main/java/org/gcnc/calculate/Application.java
@@ -2,10 +2,8 @@ package org.gcnc.calculate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
