473ac5c81d87c38740c104f017e6698f55edbf81
antonello <antonello.pastena@gmail.com>
2022-12-18 19:12:35 +0000

    extending tests

---
+++ backend/src/test/java/org/gcnc/calculate/fetcher/FetcherTest.java
@@ -0,0 +1,56 @@
+package org.gcnc.calculate.fetcher;
+
+import org.gcnc.calculate.fetcher.config.FetcherProperties;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class FetcherTest {
+    @Mock
+    private WebClient webClient;
+    @Mock
+    private WebClient.Builder builder;
+    private Fetcher fetcher;
+
+    @BeforeEach
+    void setup() {
+        FetcherProperties properties = new FetcherProperties();
+        properties.setUrl("url-test");
+        when(builder.baseUrl(anyString())).thenReturn(builder);
+        when(builder.build()).thenReturn(webClient);
+        fetcher = new Fetcher(builder, properties);
+    }
+
+    @Test
+    public void fetchTest() {
+        WebClient.RequestBodyUriSpec requestBodyUriSpec = mock(WebClient.RequestBodyUriSpec.class);
+        WebClient.RequestBodySpec requestBodySpec = mock(WebClient.RequestBodySpec.class);
+        WebClient.RequestHeadersSpec requestHeadersSpec = mock(WebClient.RequestHeadersSpec.class);
+        WebClient.ResponseSpec responseSpec = mock(WebClient.ResponseSpec.class);
+
+        when(webClient.post()).thenReturn(requestBodyUriSpec);
+        when(requestBodyUriSpec.uri(anyString())).thenReturn(requestBodySpec);
+        when(requestBodySpec.accept(any())).thenReturn(requestBodySpec);
+        when(requestBodySpec.contentType(any())).thenReturn(requestBodySpec);
+        when(requestBodySpec.body(any(BodyInserter.class))).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
+        when(responseSpec.bodyToMono(String.class)).thenReturn(Mono.just("test"));
+
+        StepVerifier.create(fetcher.fetchResponse("url-test"))
+                .expectNextMatches(response -> response.equals("test"))
+                .verifyComplete();
+    }
+
+}
